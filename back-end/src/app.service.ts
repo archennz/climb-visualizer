bd78c506cfd458c57bd9c9374247795c7edc6f3b
An Ran Chen <anranchen@Ans-MacBook-Pro.local>
2021-09-10 16:59:03 +1200

    Add back end

---
+++ back-end/src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World! An Ran was here. Josh was here too';
+  }
+}
