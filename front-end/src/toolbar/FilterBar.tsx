47a144261978c599caa0e3a8330bd7a3173ba7fa
An Ran Chen <anranchen@Ans-MacBook-Pro.local>
2021-10-01 22:54:23 +1300

    Add states to filters

--- front-end/src/toolbar/FilterBar.tsx
+++ front-end/src/toolbar/FilterBar.tsx
@@ -1,3 +1,4 @@
+import { CompareOutlined } from "@mui/icons-material";
 import {
   Button,
   Checkbox,
@@ -16,12 +17,31 @@ import * as React from "react";
 
 const FilterBar: React.FC<{drawerWidth : number}> = (props) => {
   const [gradeRange, setGradeRange] = React.useState<number[]>([3, 11]);
+  const [isPG, setIsPG] = React.useState<boolean>(false);
+  const [isX, setIsX] = React.useState<boolean>(false);
+  const [isR, setIsR] = React.useState<boolean>(false);
 
  // work out types for this  
   const handleGradeChange = (event: any, newValue: any) => {
-    console.log(newValue);
     setGradeRange(newValue);
   };
+
+
+  const gradeCommitHandler = (event: any, newValue: any) => {
+    console.log(newValue)
+  }
+  const isPGHandler = () => {
+    console.log('changed')
+    setIsPG(prev => !prev) 
+  }
+
+  const isXHandler = () => {
+    setIsX(prev => !prev)
+  }
+
+  const isRHandler = (event: React.SyntheticEvent) => {
+    setIsR(prev => !prev)
+  }
 
   const marksMin = 0;
   const marksMax = 15;
@@ -54,6 +74,7 @@ const FilterBar: React.FC<{drawerWidth : number}> = (props) => {
             marks={marksList}
             value={gradeRange}
             onChange={handleGradeChange}
+            onChangeCommitted={gradeCommitHandler}
             orientation="vertical"
             disableSwap
           ></Slider>
@@ -62,9 +83,9 @@ const FilterBar: React.FC<{drawerWidth : number}> = (props) => {
         <Box>
         <Typography variant="h6">Rating:</Typography>
         </Box>
-      <FormControlLabel control={<Checkbox />} label="PG13"></FormControlLabel>
-      <FormControlLabel control={<Checkbox />} label="X"></FormControlLabel>
-      <FormControlLabel control={<Checkbox />} label="R"></FormControlLabel>
+      <FormControlLabel control={<Checkbox />} value={isPG} label="PG13" onChange={isPGHandler}></FormControlLabel>
+      <FormControlLabel control={<Checkbox />} value={isX} label="X" onChange={isXHandler}></FormControlLabel>
+      <FormControlLabel control={<Checkbox />} value={isR} label="R" onChange={isRHandler}></FormControlLabel>
       </Stack>
       </Container>
     </Drawer>
